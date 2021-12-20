2856b5716ab900e33eccd1e82e497c7c27f404c4
Tokugero <palmueti@gmail.com>
2021-12-19 16:51:32 -0800

    Adding more documentation

--- prom-snmp-stack/snmp/scripts/randmetrics.sh
+++ prom-snmp-stack/snmp/scripts/randmetrics.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# We need to output this in a specific format so SNMP can figure out what *kind* of value we have here. In this case, we want a gauge or decimal-capable number.
 # OID
 echo ".1.3.9950.1.1"
 # TYPE
 echo "gauge"
-# VALUE
+# VALUE - This is going to just be a random number between 1 and 100. Here is what we would do to get data out of our custom monitoring devices.
 echo $(( ( RANDOM % 100 )  + 1 ))
--- tig-stack/telegraf/scripts/randmetrics.sh
+++ tig-stack/telegraf/scripts/randmetrics.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# This is going to just be a random number between 1 and 100. Here is what we would do to get data out of our custom monitoring devices.
 echo $(( ( RANDOM % 100 )  + 1 ))
